5d15558394c92b8805a2aeccd75d18e01331c1a6
Mattia Zoccarato <mattia.zoccarato@gmail.com>
2022-04-07 10:50:49 +0200

    first init

---
+++ ChironSirio/SirioChiron.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Wbm\TagManagerEcomm;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class WbmTagManagerEcomm extends Plugin
+{
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+       
+    }
+}
